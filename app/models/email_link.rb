f88a906e4983e8d978e222136cf8399ddd377b55
Dan Sowter <dan@netengine.com.au>
2013-05-28 18:49:37 +1000

    Implements EmailLink

--- app/models/email_link.rb
+++ app/models/email_link.rb
@@ -1,2 +1,50 @@
 class EmailLink
+  attr_reader :attendee
+
+  def self.confirm(attendee)
+    new(attendee).confirm
+  end
+
+  def self.pay(attendee)
+    new(attendee).pay
+  end
+
+  def self.decline(attendee)
+    new(attendee).decline
+  end
+
+  def initialize(attendee)
+    @attendee = attendee
+  end
+
+  def confirm
+    "https://#{host_name}/confirm/#{confirm_token}"
+  end
+
+  def pay
+    "https://#{host_name}/pay/#{pay_token}"
+  end
+
+  def decline
+    "https://#{host_name}/decline/#{decline_token}"
+  end
+
+  private
+
+  def host_name
+    "www.example.com"
+  end
+
+  def confirm_token
+    attendee.confirm_token
+  end
+
+  def pay_token
+    attendee.pay_token
+  end
+
+  def decline_token
+    attendee.decline_token
+  end
+
 end
--- spec/models/email_link_spec.rb
+++ spec/models/email_link_spec.rb
@@ -2,16 +2,34 @@ require 'spec_helper'
 
 describe EmailLink do
   context "when initialized with an attendee" do
-    describe "#pay" do
-      it "returns the correct url"
+    let(:attendee) { build_stubbed(:attendee) }
+
+    describe ".pay" do
+      it "returns the correct url" do
+        attendee.stub(:pay_token).and_return('ABC123')
+        url = 'https://www.example.com/pay/ABC123'
+
+        EmailLink.pay(attendee).should eq(url)
+      end
+
     end
 
-    describe "#confirm" do
-      it "returns the correct url"
+    describe ".confirm" do
+      it "returns the correct url" do
+        attendee.stub(:confirm_token).and_return('ABC123')
+        url = 'https://www.example.com/confirm/ABC123'
+
+        EmailLink.confirm(attendee).should eq(url)
+      end
     end
 
-    describe "#decline" do
-      it "returns the correct url"
+    describe ".decline" do
+      it "returns the correct url" do
+        attendee.stub(:decline_token).and_return('ABC123')
+        url = 'https://www.example.com/decline/ABC123'
+
+        EmailLink.decline(attendee).should eq(url)
+      end
     end
   end
 end
