9870859ae9a62ff000e64a461b5b89f72d73f061
Dan Sowter <dan@netengine.com.au>
2013-06-06 20:39:56 +1000

    Allows batch submission of attendees via API

--- app/controllers/api/v1/attendees_controller.rb
+++ app/controllers/api/v1/attendees_controller.rb
@@ -3,7 +3,23 @@ class Api::V1::AttendeesController < Api::V1::BaseController
     respond_with attendee_statistics, serializer: AttendeeStatisticsSerializer
   end
 
+  def create
+    render json: created_attendees, each_serializer: AttendeeSerializer, status: :created
+  end
+
   private
+
+  def created_attendees
+    [].tap do |collection|
+      supplied_attendees.each do |attendee_params|
+        collection << Attendee.create(attendee_params.slice("email_address"))
+      end
+    end
+  end
+
+  def supplied_attendees
+    params.require(:attendees)
+  end
 
   def attendee_statistics
     OpenStruct.new(attendee_state_count)
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,19 @@ class Api::V1::BaseController < ActionController::Base
   before_action :restrict_access
   respond_to :json
 
+  rescue_from ::MultiJson::LoadError do
+    respond_with_error!(:malformed_json, status: :bad_request)
+  end
+
+  def respond_with_error!(error, options = {})
+    status = options.delete(:status) || :bad_request
+    location = options.delete(:location) || root_path
+    response = ({ error: error, message: t("api.error.#{error}", options.delete(:message_params)) }).merge!(options)
+    respond_to do |format|
+      format.json { render json: response.to_json, status: status, location: location }
+    end
+  end
+
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
       token == "abc123"
