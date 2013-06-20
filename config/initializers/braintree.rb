72da8ad04c65cc064ed8bcd7b89c5f4d7a65babc
Julien Pellet <julien@netengine.com.au>
2013-06-21 09:53:11 +1000

    require braintree gem in initializer

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,3 +1,5 @@
+require 'braintree'
+
 Braintree::Configuration.environment = :sandbox
 Braintree::Configuration.merchant_id = ENV["SANDBOX_BRAINTREE_CONFIGURATION_MERCHANT_ID"]
 Braintree::Configuration.public_key = ENV["SANDBOX_BRAINTREE_CONFIGURATION_PUBLIC_KEY"]
