8df9c5a4ea8ceb8b488811b272f4099dbfe3649e
Rowan Hogan <rowan.hogan@gmail.com>
2013-07-02 15:31:23 +1000

    Added email content, updated styles to cater for different content types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,23 @@
 module ApplicationHelper
   def email_anchor(title, url)
-    email_link(title, url, "color: #FF2B06;")
+    email_link(title, url, "color: #FF2B06;", 'email-anchor')
   end
 
   def email_button(title, url)
-    email_link(title, url, "display: inline-block; padding: .25em .75em; margin: -.25em 0; font-weight: bold; text-decoration: none; color: white; background: #FF2B06; border-radius: 4px;")
+    email_link(title, url, "display: inline-block; padding: .5em 1.25em; font-weight: bold; text-decoration: none; color: white; background: #FF2B06; border-radius: 4px;", 'email-button')
+  end
+
+  def email_divider(color = '#eee')
+    content_tag(:hr, '', style: "width: 100%; border: 0; border-top: .25em solid #{color}; margin: 1.5em 0;")
+  end
+
+  def email_divider_accent
+    email_divider("#FF2B06")
   end
 
   private
   
-  def email_link(title, url, style)
-    link_to title, url, title: title, style: style
+  def email_link(title, url, style, klass)
+    link_to title, url, title: title, style: style, class: klass
   end
 end
