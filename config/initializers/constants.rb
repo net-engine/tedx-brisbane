506ce3b8958b1eb3eee5182e80bdca304f163a28
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-09-03 18:32:23 +1000

    Changes to payway.yml file.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -7,10 +7,6 @@ TICKET = OpenStruct.new(
   price_in_dollars_for_student: 50
 )
 
-
 PAYWAY = OpenStruct.new(
-  username: ENV['TEDX_PAYWAY_USERNAME'],
-  password: ENV['TEDX_PAYWAY_PASSWORD'],
-  merchant: ENV['TEDX_PAYWAY_MERCHANT'],
-  pem_file: ENV['TEDX_PAYWAY_PEM_FILE']
+  YAML.load_file("#{Rails.root}/config/payway.yml")
 )
