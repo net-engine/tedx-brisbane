cef17c0f49b6f80f8fa73f26400a65799195cb0a
erik <erikecoologic@gmail.com>
2014-08-26 12:08:55 +1000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 a = AdminUser.new
 a.email = 'lisa@woodfordia.com'
-a.password = a.password_confirmation = '20tedXbr1sban313'
+a.password = a.password_confirmation = 'ASDF'
 a.save!
 
 a = AdminUser.new
 a.email = 'team+tedx@netengine.com.au'
-a.password = a.password_confirmation = 'BridgeEpicStory2013'
+a.password = a.password_confirmation = 'asdf'
 a.save!
